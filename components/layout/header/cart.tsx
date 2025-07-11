fce22721ad44814d14e16c507d99dfc7210fb430
OtavioXavier <otavioxavier.santos@ucsal.edu.br>
2025-07-10 23:56:43 -0300

    :lipstick: reducing cart size

--- components/layout/header/cart.tsx
+++ components/layout/header/cart.tsx
@@ -8,12 +8,12 @@ interface CartProps {
 
 export default function Cart({ clasName, itens }: CartProps) {
     return (
-        <div className={clsx(clasName, "bg-white rounded-full w-10 h-10 flex items-center justify-center relative cursor-pointer")}>
+        <div className={clsx(clasName, "bg-white rounded-full w-8 h-8 flex items-center justify-center relative cursor-pointer")}>
             <span className="bg-orange-600 text-white absolute top-0 left-0 transform -translate-x-1/2 -translate-y-1/2 rounded-full min-w-6 min-h-6 text-sm flex items-center justify-center">
                 {itens.toString()}
             </span>
             <div className="text-emerald-500">
-                <ShoppingCart />
+                <ShoppingCart size={16} />
             </div>
         </div>
 
